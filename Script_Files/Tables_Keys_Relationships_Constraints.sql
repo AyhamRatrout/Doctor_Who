dea90fe1efe937b4010bb8f6eaf951565dbb63b0
AyhamRatrout <arr90@case.edu>
2021-07-22 22:40:26 +0300

    Fix Formatting

--- Script_Files/Tables_Keys_Relationships_Constraints.sql
+++ Script_Files/Tables_Keys_Relationships_Constraints.sql
@@ -22,31 +22,31 @@ CREATE TABLE tblDoctor(
 						);
 
 CREATE TABLE tblCompanion(
-							CompanionId INT NOT NULL PRIMARY KEY,
-							CompanionName VARCHAR(150) NOT NULL,
-							WhoPlayed VARCHAR(150) NOT NULL
-							);
+						  CompanionId INT NOT NULL PRIMARY KEY,
+						  CompanionName VARCHAR(150) NOT NULL,
+						  WhoPlayed VARCHAR(150) NOT NULL
+						 );
 
 CREATE TABLE tblEpisode (
-							EpisodeId INT NOT NULL PRIMARY KEY,
-							SeriesNumber INT NOT NULL,
-							EpisodeNumber INT NOT NULL,
-							EpisodeType VARCHAR(115) DEFAULT NULL,
-							Title VARCHAR(150) NOT NULL,
-							EpisodeDate Date DEFAULT NULL,
-							AuthorId INT NOT NULL REFERENCES tblAuthor (AuthorId),
-							DoctorId INT NOT NULL REFERENCES tblDoctor (DoctorId),
-							Notes VARCHAR(650) DEFAULT NULL
-							);
+						 EpisodeId INT NOT NULL PRIMARY KEY,
+						 SeriesNumber INT NOT NULL,
+						 EpisodeNumber INT NOT NULL,
+						 EpisodeType VARCHAR(115) DEFAULT NULL,
+						 Title VARCHAR(150) NOT NULL,
+						 EpisodeDate Date DEFAULT NULL,
+						 AuthorId INT NOT NULL REFERENCES tblAuthor (AuthorId),
+						 DoctorId INT NOT NULL REFERENCES tblDoctor (DoctorId),
+						 Notes VARCHAR(650) DEFAULT NULL
+						);
 
 CREATE TABLE tblEpisodeCompanion(
-									EpisodeCompanionId INT NOT NULL PRIMARY KEY,
-									EpisodeId INT NOT NULL REFERENCES tblEpisode (EpisodeId),
-									CompanionId INT NOT NULL REFERENCES tblCompanion (CompanionId)
-									);
+								 EpisodeCompanionId INT NOT NULL PRIMARY KEY,
+								 EpisodeId INT NOT NULL REFERENCES tblEpisode (EpisodeId),
+								 CompanionId INT NOT NULL REFERENCES tblCompanion (CompanionId)
+								 );
 
 CREATE TABLE tblEpisodeEnemy (
-								EpisodeEnemyId INT NOT NULL PRIMARY KEY,
-								EpisodeId INT NOT NULL REFERENCES tblEpisode (EpisodeId),
-								EnemyId INT NOT NULL REFERENCES tblEnemy (EnemyId)
-								);
+							  EpisodeEnemyId INT NOT NULL PRIMARY KEY,
+							  EpisodeId INT NOT NULL REFERENCES tblEpisode (EpisodeId),
+							  EnemyId INT NOT NULL REFERENCES tblEnemy (EnemyId)
+							 );
